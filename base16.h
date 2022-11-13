12405e0bde9d42c56e8e85c58b62c81025c85500
Jakub Vlk <kuba24862vlk@gmail.com>
2022-11-13 23:43:01 +0100

    Velká třesk

--- base16.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by jvlk on 11.11.22.
-//
-
-#ifndef BASE16_H
-#define BASE16_H
-unsigned char *tobase16(const unsigned char *in, int len);
-unsigned char *frombase16(const unsigned char *in, int len);
-#endif //BASE16_H
--- client.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by jvlk on 19.10.22.
-//
-
-#ifndef DNSTUNNEL_SERVER_H
-#define DNSTUNNEL_SERVER_H
-
-#include "common.h"
-#endif //DNSTUNNEL_SERVER_H
